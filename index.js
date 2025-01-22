f4f52e621b5cec02ad24bc38a71b929821e50e1b
Nina Simmons <pkpbabies@gmail.com>
2025-01-22 14:42:00 -0500

    Modify stlyes and remove nav bar

---
+++ index.js
@@ -0,0 +1,28 @@
+// First up, let's create a new list item and store it in a variable.
+let newLi = document.createElement('li')
+
+// Now let's update the text content of that list item.
+newLi.textContent = 'Flaxseed'
+
+// And finally, let's add that list item as a child of the ul.
+ul.appendChild("Flaxseed")
+
+// First up, let's create a new list item and store it in a variable.
+//let newListItem = document.createElement('li');
+
+// Now let's update the text content of that list item.
+//newListItem.textContent = '2 tablespoons of Flaxseed';
+
+// And finally, let's add that list item as a child of the ul.
+//document.querySelector('ul').appendChild(newListItem);
+
+// Create interactive elements using mybox method and addeventlistener
+
+//const myBox= document.getElementsById("myBox");
+
+//function changeColor(event) {
+  //  console.log(event);
+//}
+//myBox.addEventlistener(click, callback);
+
+
