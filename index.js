c2a09db55abcad81a0cbe2be297d67537c0fc6fe
Nina Simmons <pkpbabies@gmail.com>
2025-01-23 11:49:29 -0500

    Add interactive buttons and styling

--- index.js
+++ index.js
@@ -1,28 +1,64 @@
-// First up, let's create a new list item and store it in a variable.
-let newLi = document.createElement('li')
+// select an element
+console.log("hello👋👋👋👋👋");
+
+// // First up, let's create a new list item and store it in a variable.
+//let newLi = document.createElement('li')
 
 // Now let's update the text content of that list item.
-newLi.textContent = 'Flaxseed'
+//newLi.textContent = 'Flaxseed'
 
 // And finally, let's add that list item as a child of the ul.
-ul.appendChild("Flaxseed")
+//ul.appendChild("Flaxseed")
 
 // First up, let's create a new list item and store it in a variable.
-//let newListItem = document.createElement('li');
+let newListItem = document.createElement('li');
 
 // Now let's update the text content of that list item.
-//newListItem.textContent = '2 tablespoons of Flaxseed';
+newListItem.textContent = '2 tablespoons of Flaxseed';
 
 // And finally, let's add that list item as a child of the ul.
-//document.querySelector('ul').appendChild(newListItem);
+document.querySelector('ul').appendChild(newListItem);
 
 // Create interactive elements using mybox method and addeventlistener
 
-//const myBox= document.getElementsById("myBox");
+ const myBox = document.getElementById("myBox");
+ const myButton = document.getElementById("myButton");
 
-//function changeColor(event) {
-  //  console.log(event);
+// function changeColor(event) {
 //}
-//myBox.addEventlistener(click, callback);
+
+myButton.addEventListener("click", event => {
+    myBox.style.backgroundColor = "beige";
+    myBox.textContent = "It's so good, make you wanna slap yo momma!";
+    })
+myButton.addEventListener("mouseover", event => {
+    myBox.style.backgroundColor = "tan";
+    myBox.textContent = "It's so good, make you wanna slap yo momma!";
+},);
+
+myButton.addEventListener("mouseout", event => {
+    myBox.style.backgroundColor = "brown";
+    myBox.textContent = "Mmm Mmm Good!";
+});
 
 
+
+//const myUlLi = document.getElementById("myUli");
+
+//let checkbox = document.createElement("input");
+
+//checkbox.type = "checkbox";
+//checkbox.name = "name";
+//checkbox.value = "value";
+//checkbox.id = "id";
+
+//var label = document.createElement("label");
+
+//label.htmlFor = "id";
+
+//label.appendChild(
+ //   document.createTextNode("This create the laabel for checkbox.")
+//);
+
+//myUlLi.appendChild(checkbox);
+//myUlLi.appendChild(label);
